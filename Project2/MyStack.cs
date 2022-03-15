673e4574ba00a3b47eb5a01228fca01e5749172d
AmetoX <67999655+AmetoX@users.noreply.github.com>
2022-03-15 11:01:14 +0200

    Minor changes!

---
+++ Project2/MyStack.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Project2
+{
+    internal class MyStack
+    {
+        int[] data;
+        private int _capacity = 16;
+        private int _count = 0;
+
+        public int Capacity { get { return _capacity; } }
+        public int  Count { get { return _count; } }
+        public MyStack()
+        {
+            data = new int[_capacity];
+        }
+
+        internal void Push(int v)
+        {
+           
+        }
+    }
+}
---
+++ Project2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack s = new MyStack();
+            s.Push(1);
+            Console.WriteLine($"Capacitatea stivei este: {s.Capacity} ");
+            Console.WriteLine($"Numarul de elemente din stiva este: {s.Count}");
+        }
+    }
+}
