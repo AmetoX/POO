d13e92be9a293a9cacac8bc4a9ec0882bf4159ee
AmetoX <67999655+AmetoX@users.noreply.github.com>
2022-03-22 12:49:12 +0200

    lab 22.03.2022

---
+++ Project3(lab22.03)/Point.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project3_lab22._03_
+{
+    internal partial class Point
+    {
+        #region Properties
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        //public Color Color { get; set; }
+        #endregion
+        #region Contructors
+        public Point() : this (0, 0)
+        {
+
+        }
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+            Console.WriteLine($"Am creat un obiect de timp Point ({X}, {Y})");
+        }
+
+        public Point(int x) : this(x, 0) //constructor telescopic
+        {
+
+        }
+        #endregion
+        #region Override
+        public override string ToString()
+        {
+            return $"({X},{Y})";
+        }
+        #endregion
+        #region Methods
+        public double GetDistanceToOrigin()
+        {
+            return Math.Sqrt(X * X + Y * Y);
+        }
+        public double GetDistanceTo(Point to)
+        {
+            return Math.Sqrt(Math.Pow(this.X - to.X, 2)+ 
+                                Math.Pow(this.Y - to.Y, 2));
+        }
+        public void MoveBy(double dx, double dy)
+        {
+            X += dx;
+            Y += dy;
+        }
+        //360 grade = 2 pii radiani 
+        //rotire din x1 y1 in x2 y2 cu alfa grade
+        //x2 = xcosalfa -ysinalfa
+        //y2 = xsinalfa +ycosalfa
+        public void Rotire(double angle)
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ Project3(lab22.03)/Program.cs
@@ -0,0 +1,19 @@
+﻿using Project3_lab22._03_;
+
+Console.WriteLine("-Hello-");
+Console.WriteLine();
+
+Point p1 = new Point { X = 3.0, Y = 4.0 };
+Point p2 = new Point();
+Point p3 = new Point(3, 4);
+
+Console.WriteLine(p1);
+
+Point p4 = new Point(5);
+//Point p5 = new Point(5, 6) { Color = Co};
+Console.WriteLine();
+Console.WriteLine($"Distanta de la origine in pct {p1} este {p1.GetDistanceToOrigin()}");
+Console.WriteLine();
+Point p6 = new Point(1,1);
+p6.MoveBy(2, 2);
+Console.WriteLine($"Moved point {p6}.");
