c0e9e31ad1990676c24854c607265cf7fc4a2978
AmetoX <67999655+AmetoX@users.noreply.github.com>
2022-04-12 12:16:26 +0300

    lab 12.04 (MultiCounter)

---
+++ Lab_12_04/Counter.cs
@@ -0,0 +1,49 @@
+﻿internal class Counter
+{
+    private string id;
+    private int count;
+    private Counter? nextCounter;
+
+    public Counter? NextCounter
+    {
+        get { return nextCounter; }
+        set { nextCounter = value; }
+    }
+    public int Count
+    {
+        get
+        {
+            return count;//propietate read-only
+        }         
+    }
+
+    public Counter(string id)
+    {
+        this.id = id;
+        this.count = 0;//initializarea asta oricum se face
+        nextCounter = null;
+    }
+
+    internal void Increment()
+    {
+        count++;
+        if(count == 10)
+        {
+            count = 0;
+            if (nextCounter != null)
+            {
+                nextCounter.Increment();
+            }
+        }
+    }
+
+    internal void Reset()
+    {
+        count = 0;
+    }
+
+    public override string ToString()
+    {
+        return $"[Counter <{id}>: {count}]";
+    }
+}
---
+++ Lab_12_04/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine();//-MultiCounter-
+
+Counter heads = new Counter("heads");
+Counter tails = new Counter("tails");
+Random random = new Random();
+
+for(int i = 0; i < 1000; i++)
+{
+    switch (random.Next(2))
+    {
+        case 0:
+            heads.Increment();
+            break;
+        case 1:
+            tails.Increment();
+            break;
+        default:
+            break;
+    }
+}
+
+Console.WriteLine($"Heads: {heads.Count}, Tails: {tails.Count}.");
+Console.WriteLine(heads);
+Console.WriteLine(tails);
+Console.WriteLine();
+
+heads.Reset();
+tails.Reset();
+
+Console.WriteLine($"Heads: {heads.Count}, Tails: {tails.Count}.");
+Console.WriteLine(heads);
+Console.WriteLine(tails);
+Console.WriteLine();
+
+MultiCounter mc = new MultiCounter(4);
+
+for(int i = 0; i < 10000; i++)
+{
+    mc.Increment();
+    Console.WriteLine(mc);
+}
